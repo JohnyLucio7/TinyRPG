ca5edf1a5584e21a267f1633e389dbb91f9ee0bf
johny.lucio.707 <johny.lucio.707@ufrn.edu.br>
2022-08-12 19:26:49 -0300

    update

--- TinyRPG/Assets/Scripts/Enemies/IASlime.cs
+++ TinyRPG/Assets/Scripts/Enemies/IASlime.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class IASlime : MonoBehaviour
 {
     private new Rigidbody2D rigidbody;
+    private bool isLockPlayer;
+
     public Vector2 moveDirection;
+
+    public LayerMask whatIsPlayer;
 
     [Range(0f, 10f)] public float maxTimeToMove;
     [Range(0f, 10f)] public float minTimeToMove;
     [Range(0f, 10f)] public float waitMax;
     [Range(0f, 10f)] public float waitMin;
     [Range(0.01f, 1f)] public float speed;
+    [Range(0.01f, 1f)] public float radiusVision;
 
     // Start is called before the first frame update
     void Start()
@@ -21,21 +26,33 @@ public class IASlime : MonoBehaviour
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
         rigidbody.velocity = moveDirection * speed;
+
+        if (isLockPlayer) moveDirection = Vector3.Normalize(PlayerController.instance.transform.position - transform.position);
+    }
+
+    private void FixedUpdate()
+    {
+        isLockPlayer = Physics2D.OverlapCircle(transform.position, radiusVision, whatIsPlayer);
     }
 
     IEnumerator moveSlime()
     {
-        yield return new WaitForSeconds(Random.Range(waitMin, waitMax));
-        int x = Random.Range(-1, 2);
-        int y = Random.Range(-1, 2);
-
-        moveDirection = new Vector2(x, y);
-
-        yield return new WaitForSeconds(Random.Range(minTimeToMove, maxTimeToMove));
         moveDirection = new Vector2(0, 0);
+
+        yield return new WaitForSeconds(Random.Range(waitMin, waitMax));
+
+        if (!isLockPlayer)
+        {
+            int x = Random.Range(-1, 2);
+            int y = Random.Range(-1, 2);
+
+            moveDirection = new Vector2(x, y);
+
+            yield return new WaitForSeconds(Random.Range(minTimeToMove, maxTimeToMove));
+        }
 
         StartCoroutine("moveSlime");
     }
